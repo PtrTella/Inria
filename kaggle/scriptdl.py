264e484f11a9b8506550c1d45a2347801306dab6
Tella <tellarinipietro@gmail.com>
2025-08-24 21:38:06 +0200

    nothing

--- kaggle/scriptdl.py
+++
@@ -1,8 +0,0 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-
-api = KaggleApi()
-api.authenticate()  
-kernels = api.kernels_list(user='ptrtella')
-print(f"Found {len(kernels)} kernels in the 'ptrtella/rawclip' dataset.")
-print(f"Listing kernels: {[kernel.ref for kernel in kernels]}")
-api.kernels_output('ptrtella/rawclip', path='./kaggle_output', force=True) 
